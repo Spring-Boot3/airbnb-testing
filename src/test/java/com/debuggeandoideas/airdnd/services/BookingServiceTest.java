c00beff1e57aacb529c9a0314f9a61f493dd8cb8
HectorRoG <hectorr9577@gmail.com>
2025-04-13 18:12:36 -0600

    Argument matchers

--- src/test/java/com/debuggeandoideas/airdnd/services/BookingServiceTest.java
+++ src/test/java/com/debuggeandoideas/airdnd/services/BookingServiceTest.java
@@ -3,6 +3,7 @@ package com.debuggeandoideas.airdnd.services;
 import com.debuggeandoideas.airdnd.Services.BookingService;
 import com.debuggeandoideas.airdnd.Services.PaymentService;
 import com.debuggeandoideas.airdnd.Services.RoomService;
+import com.debuggeandoideas.airdnd.dto.BookingDto;
 import com.debuggeandoideas.airdnd.helpers.MailHelper;
 import com.debuggeandoideas.airdnd.repositories.BookingRepository;
 import com.debuggeandoideas.airdnd.utils.DataDummy;
@@ -12,6 +13,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -37,6 +40,25 @@ public class BookingServiceTest {
                 .thenReturn(DataDummy.default_rooms_list);
 
         assertEquals(14, bookingService.getAvailablePlaceCount());
+    }
+
+    @Test
+    @DisplayName("booking should works")
+    void booking() {
+        final var roomId = UUID.randomUUID().toString();
+        //Arguments matcher strict
+//        when(roomServiceMock.findAvailableRoom(DataDummy.default_booking_req_1))
+//                .thenReturn(DataDummy.default_rooms_list.get(0));
+//        when(bookingRepositoryMock.save(DataDummy.default_booking_req_1))
+//                .thenReturn(roomId);
+        //Arguments matcher any
+        when(roomServiceMock.findAvailableRoom(any(BookingDto.class)))
+                .thenReturn(DataDummy.default_rooms_list.get(0));
+        when(bookingRepositoryMock.save(any(BookingDto.class)))
+                .thenReturn(roomId);
+        var result = bookingService.booking(DataDummy.default_booking_req_3);
+        assertEquals(roomId, result);
+
     }
 
 
--- src/test/java/com/debuggeandoideas/airdnd/utils/DataDummy.java
+++ src/test/java/com/debuggeandoideas/airdnd/utils/DataDummy.java
@@ -1,7 +1,9 @@
 package com.debuggeandoideas.airdnd.utils;
 
+import com.debuggeandoideas.airdnd.dto.BookingDto;
 import com.debuggeandoideas.airdnd.dto.RoomDto;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +33,34 @@ public class DataDummy {
             new RoomDto("D", 2),
             new RoomDto("E", 2),
             new RoomDto("F", 3)
+    );
+
+    public static final List<RoomDto> silgle_rooms_list = List.of(
+            new RoomDto("A", 5)
+    );
+
+    public static final BookingDto default_booking_req_1 = new BookingDto(
+            "18318",
+            LocalDate.of(2023, 06, 10),
+            LocalDate.of(2023, 06, 20),
+            2,
+            false
+    );
+
+    public static final BookingDto default_booking_req_2 = new BookingDto(
+            "3784193",
+            LocalDate.of(2023, 06, 10),
+            LocalDate.of(2023, 06, 26),
+            2,
+            true
+    );
+
+    public static final BookingDto default_booking_req_3 = new BookingDto(
+            "48392392",
+            LocalDate.of(2023, 05, 10),
+            LocalDate.of(2023, 06, 26),
+            2,
+            false
     );
 
 }
